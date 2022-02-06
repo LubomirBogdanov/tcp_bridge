05196e8d019790674139713e09de2491502ba788
lbogdanov <lbogdanov@tu-sofia.bg>
2022-02-06 15:32:08 +0200

    hex: updated the hex dump friend function.

--- tcp_bridge/common.cpp
+++ tcp_bridge/common.cpp
@@ -3,17 +3,9 @@
 using namespace std;
 
 void display_byte_array(QByteArray &hex_data){
-    QChar hex_value;
-
     qDebug()<<"======================================";
-    for(int i = 0; i < hex_data.size(); i++){
-        hex_value = hex_data.at(i);
-        cout << QString::asprintf("%02X ", hex_value.toLatin1()).toStdString();
-    }
-    cout <<"\n--------------------------------------\n";
-    for(int i = 0; i < hex_data.size(); i++){
-        hex_value = hex_data.at(i);
-        cout << QString::asprintf("%c", hex_value.toLatin1()).toStdString();
-    }
+    qDebug()<<hex_data.toHex(' ');
+    qDebug() <<"\n--------------------------------------\n";
+    qDebug()<<hex_data;
     qDebug()<<"\n======================================\n";
 }
